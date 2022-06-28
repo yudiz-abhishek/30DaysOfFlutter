c4829342c8922b3eac467fb1424262e79d305918
Abhishek Bakhai <abhishek.b@yudiz.com>
2022-06-28 21:39:50 +0530

    InkWell,AnimatedContainer,setState,BoxDecoration

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeBtn = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +26,7 @@ class LoginPage extends StatelessWidget {
                 height: 20.0,
               ),
               Text(
-                "Welcome",
+                "Welcome, ${name}",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
@@ -36,6 +44,12 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter Username", labelText: "Username"),
+                      //For OnChange Value while user typing add to welcome text
+                      onChanged: (value) {
+                        name = value;
+                        //For change state (work as a reload particalur state)
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       //For Seucre text default => False
@@ -47,12 +61,49 @@ class LoginPage extends StatelessWidget {
                       height: 40.0,
                     ),
 
-                    ElevatedButton(
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                      onPressed: () {
+                    // ElevatedButton(
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    // )
+                    //For Container with Clickable event
+                    InkWell(
+                      onTap: () async {
+                        //For changing button state
+                        setState(() {
+                          //For Container Width Change for animation
+                          changeBtn = true;
+                        });
+
+                        //Same as swift when we are using await func must be async
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeBtn ? 50 : 150,
+                        height: 50,
+                        //If we are using decoration at that time we need to give color in decoration
+                        // color: Colors.deepPurple,
+                        alignment: Alignment.center,
+                        child: changeBtn
+                            ? Icon(Icons.done, color: Colors.white)
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            // shape:
+                            //     changeBtn ? BoxShape.circle : BoxShape.rectangle,
+                            borderRadius:
+                                BorderRadius.circular(changeBtn ? 50 : 8)),
+                      ),
                     )
                   ],
                 ),
