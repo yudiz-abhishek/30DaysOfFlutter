d2293b0c52cdaa289e248fdf812df8d3748ce3f3
Abhishek Bakhai <abhishek.b@yudiz.com>
2022-07-21 15:53:53 +0530

    VelocityX,ElevatedButton,ButtonBar(StadiumBorder)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:velocity_x/velocity_x.dart';
+
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
-import 'package:flutter/services.dart';
-import 'dart:convert';
+import 'package:flutter_catalog/widgets/themes.dart';
+
 import '../widgets/drawer.dart';
 
 class HomePage extends StatefulWidget {
@@ -37,6 +43,172 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: MyTheme.creamColor,
+        body: SafeArea(
+          child: Container(
+            padding: Vx.m32, //For all EdgeInsets
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CatalogHeader(),
+                if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                  CatalogList().expand() //For Expending list
+                else
+                  Center(
+                    child: CircularProgressIndicator(),
+                  )
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+//For Header
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Trending products".text.xl2.make(),
+      ],
+    );
+  }
+}
+
+//For Listing Items
+class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: CatalogModel.items.length,
+        itemBuilder: (context, index) {
+          final catalog = CatalogModel.items[index];
+          return CatalogItem(catelog: catalog);
+        });
+  }
+}
+
+//For One Item
+class CatalogItem extends StatelessWidget {
+  const CatalogItem({
+    Key? key,
+    required this.catelog,
+  })  : assert(catelog != null),
+        super(key: key);
+
+  final Item catelog;
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+            child: Row(
+      children: [
+        CatalogImage(
+          image: catelog.image,
+        ),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catelog.name.text.lg //lg => textScaleFactor
+                .color(MyTheme.darkBluishColor)
+                .bold
+                .make(),
+            catelog.desc.text
+                .textStyle(context.captionStyle) //for Caption Style
+                .make(),
+            10.heightBox, //For Spacing Empty Box
+            ButtonBar(
+              alignment:
+                  MainAxisAlignment.spaceBetween, //For Equal Spacing for both
+              buttonPadding: EdgeInsets
+                  .zero, //For Right Padding //For Remove Padding (EdgeInsets.zero)
+              // For Button Bar
+              children: [
+                "\$${catelog.price}".text.bold.xl.make(),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(MyTheme.darkBluishColor),
+                      shape: MaterialStateProperty.all(
+                          StadiumBorder()), //For Give the shpae for Button
+                    ),
+                    child: "Buy".text.make())
+              ],
+            ).pOnly(right: 8)
+          ],
+        ))
+      ],
+    ))
+        .white
+        .roundedLg //RoundedLg => Large (Velocity_X)
+        .square(150)
+        .make()
+        .py16();
+  }
+}
+
+//For Image
+class CatalogImage extends StatelessWidget {
+  final String image;
+
+  const CatalogImage({
+    Key? key,
+    required this.image,
+  })  : assert(image != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .rounded
+        .p8 //Padding
+        .color(MyTheme.creamColor)
+        .make()
+        .p16()
+        .w40(context); //For Width half wHalf same as for width 40
+  }
+}
+
+//For ListView
+/*ListView.builder(
+                itemCount: CatalogModel.items.length,
+                // itemBuilder: (content, index) {
+                //   return ItemWidget(
+                //     item: CatalogModel.items[index],
+                //   );
+                // })
+                //Using Shorhand operator
+                itemBuilder: (content, index) => ItemWidget(
+                      item: CatalogModel.items[index],
+                    )) */
+
+//Only For this Page but give it to whole app we need to give it in themeData appBarTheme, bottomAppBarColor, etc
+// appBar: AppBar(
+//   backgroundColor: Colors.white,
+//   elevation: 0.0,
+//   iconTheme: IconThemeData(color: Colors.black),
+//   title: Text(
+//     "Catalog App",
+//     style: TextStyle(color: Colors.black),
+//   ),
+// )
+
+//With Out Velocity_X
+/*@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -91,29 +263,4 @@ class _HomePageState extends State<HomePage> {
       ),
       drawer: MyDrawer(),
     );
-  }
-}
-
-//For ListView
-/*ListView.builder(
-                itemCount: CatalogModel.items.length,
-                // itemBuilder: (content, index) {
-                //   return ItemWidget(
-                //     item: CatalogModel.items[index],
-                //   );
-                // })
-                //Using Shorhand operator
-                itemBuilder: (content, index) => ItemWidget(
-                      item: CatalogModel.items[index],
-                    )) */
-
-//Only For this Page but give it to whole app we need to give it in themeData appBarTheme, bottomAppBarColor, etc
-// appBar: AppBar(
-//   backgroundColor: Colors.white,
-//   elevation: 0.0,
-//   iconTheme: IconThemeData(color: Colors.black),
-//   title: Text(
-//     "Catalog App",
-//     style: TextStyle(color: Colors.black),
-//   ),
-// )
+  } */
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -7,7 +7,7 @@ class MyTheme {
       //Auto Switch Colors using dart collections
       primarySwatch: Colors.deepPurple,
       //For Allover App Font
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0.0,
@@ -18,4 +18,8 @@ class MyTheme {
   //Dark Theme
   static ThemeData darkTheme(BuildContext context) =>
       ThemeData(brightness: Brightness.dark);
+
+  //Colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
