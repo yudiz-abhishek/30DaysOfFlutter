8e9dce4d612121dbb1feab2aea68d7c42133eff3
Abhishek Bakhai <abhishek.b@yudiz.com>
2022-07-21 11:47:44 +0530

    decodedData, List Mapping

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
-
 import '../widgets/drawer.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,7 +13,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final int days = 30;
   final String name = "Abhishek";
-
+  //For Dummy List with same items
+  final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
   @override
   void initState() {
     super.initState();
@@ -25,21 +26,8 @@ class _HomePageState extends State<HomePage> {
         await rootBundle.loadString("assets/files/catelog.json");
     final decodedData = jsonDecode(cateloagJosn);
     var productData = decodedData["products"];
+    print(productData);
   }
-=======
-import 'package:flutter_catalog/models/catalog.dart';
-import 'package:flutter_catalog/widgets/item_widget.dart';
-
-import '../widgets/drawer.dart';
-
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Abhishek";
-  @override
-  Widget build(BuildContext context) {
-    //For Dummy List with same items
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
->>>>>>> bb850c3970ac4d0974fc136de0f11634496c7575
 
   @override
   Widget build(BuildContext context) {
